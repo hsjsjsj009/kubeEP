ccf08a45c75457cc6fb20f80add090f789cc4761
Dipta Dwiyantoro <dipta.dwiyantoro@tokopedia.com>
2022-02-20 11:35:45 +0700

    Update hpa list data cache time

--- kubeEP-BE/internal/repository/k8s_hpa.go
+++ kubeEP-BE/internal/repository/k8s_hpa.go
@@ -16,8 +16,18 @@ import (
 )
 
 type K8sHPA interface {
-	GetAllV1HPA(ctx context.Context, client kubernetes.Interface, namespace v1Core.Namespace, clusterID uuid.UUID) ([]v1Autoscale.HorizontalPodAutoscaler, error)
-	GetAllV2HPA(ctx context.Context, client kubernetes.Interface, namespace v1Core.Namespace, clusterID uuid.UUID) ([]v2Autoscale.HorizontalPodAutoscaler, error)
+	GetAllV1HPA(
+		ctx context.Context,
+		client kubernetes.Interface,
+		namespace v1Core.Namespace,
+		clusterID uuid.UUID,
+	) ([]v1Autoscale.HorizontalPodAutoscaler, error)
+	GetAllV2HPA(
+		ctx context.Context,
+		client kubernetes.Interface,
+		namespace v1Core.Namespace,
+		clusterID uuid.UUID,
+	) ([]v2Autoscale.HorizontalPodAutoscaler, error)
 }
 
 type k8sHPA struct {
@@ -30,9 +40,21 @@ func newK8sHPA(redisClient *redis.Client) K8sHPA {
 	}
 }
 
-func (h *k8sHPA) GetAllV1HPA(ctx context.Context, client kubernetes.Interface, namespace v1Core.Namespace, clusterID uuid.UUID) ([]v1Autoscale.HorizontalPodAutoscaler, error) {
+const (
+	HPACacheTime = 30 * time.Second
+)
+
+func (h *k8sHPA) GetAllV1HPA(
+	ctx context.Context,
+	client kubernetes.Interface,
+	namespace v1Core.Namespace,
+	clusterID uuid.UUID,
+) ([]v1Autoscale.HorizontalPodAutoscaler, error) {
 	key := fmt.Sprintf("hpa_v1_list_cluster_%s", clusterID)
-	if redisResponse := h.redisClient.Get(ctx, key); redisResponse.Err() != nil {
+	if redisResponse := h.redisClient.Get(
+		ctx,
+		key,
+	); redisResponse.Err() != nil {
 		var HPAList v1Autoscale.HorizontalPodAutoscalerList
 		b, err := redisResponse.Bytes()
 		if err == nil {
@@ -52,18 +74,31 @@ func (h *k8sHPA) GetAllV1HPA(ctx context.Context, client kubernetes.Interface, n
 			v1Option.ListOptions{},
 		)
 	if err != nil {
-		_ = h.redisClient.Set(ctx, key, errorConstant.HPAListError, 10*time.Second).Err()
+		_ = h.redisClient.Set(
+			ctx,
+			key,
+			errorConstant.HPAListError,
+			HPACacheTime,
+		).Err()
 		return nil, err
 	}
 	if b, err := data.Marshal(); err == nil {
-		_ = h.redisClient.Set(ctx, key, b, 10*time.Second).Err()
+		_ = h.redisClient.Set(ctx, key, b, HPACacheTime).Err()
 	}
 	return data.Items, nil
 }
 
-func (h *k8sHPA) GetAllV2HPA(ctx context.Context, client kubernetes.Interface, namespace v1Core.Namespace, clusterID uuid.UUID) ([]v2Autoscale.HorizontalPodAutoscaler, error) {
+func (h *k8sHPA) GetAllV2HPA(
+	ctx context.Context,
+	client kubernetes.Interface,
+	namespace v1Core.Namespace,
+	clusterID uuid.UUID,
+) ([]v2Autoscale.HorizontalPodAutoscaler, error) {
 	key := fmt.Sprintf("hpa_v2_list_cluster_%s", clusterID)
-	if redisResponse := h.redisClient.Get(ctx, key); redisResponse.Err() != nil {
+	if redisResponse := h.redisClient.Get(
+		ctx,
+		key,
+	); redisResponse.Err() != nil {
 		var HPAList v2Autoscale.HorizontalPodAutoscalerList
 		b, err := redisResponse.Bytes()
 		if err == nil {
@@ -83,11 +118,16 @@ func (h *k8sHPA) GetAllV2HPA(ctx context.Context, client kubernetes.Interface, n
 			v1Option.ListOptions{},
 		)
 	if err != nil {
-		_ = h.redisClient.Set(ctx, key, errorConstant.HPAListError, 10*time.Second).Err()
+		_ = h.redisClient.Set(
+			ctx,
+			key,
+			errorConstant.HPAListError,
+			HPACacheTime,
+		).Err()
 		return nil, err
 	}
 	if b, err := data.Marshal(); err == nil {
-		_ = h.redisClient.Set(ctx, key, b, 10*time.Second).Err()
+		_ = h.redisClient.Set(ctx, key, b, HPACacheTime).Err()
 	}
 	return data.Items, nil
 }

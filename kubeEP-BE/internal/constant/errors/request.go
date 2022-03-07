07e2a0e48d755229d1c263409596cb59f57ce2ee
Dipta Dwiyantoro <dipta.dwiyantoro@tokopedia.com>
2022-03-07 13:58:15 +0700

    Update get event detail endpoint

--- kubeEP-BE/internal/constant/errors/request.go
+++ kubeEP-BE/internal/constant/errors/request.go
@@ -3,4 +3,6 @@ package errorConstant
 const (
 	InvalidRequestBody = "invalid request body"
 	InvalidQueryParam  = "invalid query param"
+	ParamRequired      = "%s param required"
+	ParamInvalid       = "%s param invalid"
 )

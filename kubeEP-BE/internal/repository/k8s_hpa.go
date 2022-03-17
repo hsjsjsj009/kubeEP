a8ef76338155cdae90944332823ca905204cd5e9
Dipta Dwiyantoro <dipta.dwiyantoro@tokopedia.com>
2022-03-17 08:23:21 +0700

    Add update hpa to repository

--- kubeEP-BE/internal/repository/k8s_hpa.go
+++ kubeEP-BE/internal/repository/k8s_hpa.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/go-redis/redis/v8"
 	"github.com/google/uuid"
+	"github.com/hsjsjsj009/kubeEP/kubeEP-BE/internal/constant"
 	errorConstant "github.com/hsjsjsj009/kubeEP/kubeEP-BE/internal/constant/errors"
 	v1Autoscale "k8s.io/api/autoscaling/v1"
 	"k8s.io/api/autoscaling/v2beta1"
@@ -35,6 +36,27 @@ type K8sHPA interface {
 		namespace v1Core.Namespace,
 		clusterID uuid.UUID,
 	) ([]v2beta1.HorizontalPodAutoscaler, error)
+	UpdateV2beta1HPA(
+		ctx context.Context,
+		client kubernetes.Interface,
+		namespace v1Core.Namespace,
+		clusterID uuid.UUID,
+		hpa *v2beta1.HorizontalPodAutoscaler,
+	) (*v2beta1.HorizontalPodAutoscaler, error)
+	UpdateV1HPA(
+		ctx context.Context,
+		client kubernetes.Interface,
+		namespace v1Core.Namespace,
+		clusterID uuid.UUID,
+		hpa *v1Autoscale.HorizontalPodAutoscaler,
+	) (*v1Autoscale.HorizontalPodAutoscaler, error)
+	UpdateV2beta2HPA(
+		ctx context.Context,
+		client kubernetes.Interface,
+		namespace v1Core.Namespace,
+		clusterID uuid.UUID,
+		hpa *v2beta2.HorizontalPodAutoscaler,
+	) (*v2beta2.HorizontalPodAutoscaler, error)
 }
 
 type k8sHPA struct {
@@ -181,4 +203,83 @@ func (h *k8sHPA) GetAllV2beta1HPA(
 		_ = h.redisClient.Set(ctx, key, b, HPACacheTime).Err()
 	}
 	return data.Items, nil
+}
+
+func (h *k8sHPA) UpdateV1HPA(
+	ctx context.Context,
+	client kubernetes.Interface,
+	namespace v1Core.Namespace,
+	clusterID uuid.UUID,
+	hpa *v1Autoscale.HorizontalPodAutoscaler,
+) (*v1Autoscale.HorizontalPodAutoscaler, error) {
+	key := fmt.Sprintf("hpa_v1_list_cluster_%s_ns_%s", clusterID, namespace.Name)
+	data, err := client.
+		AutoscalingV1().
+		HorizontalPodAutoscalers(namespace.Name).
+		Update(
+			ctx,
+			hpa,
+			v1Option.UpdateOptions{FieldManager: constant.K8sHPAUpdateFieldManager},
+		)
+	if err != nil {
+		return nil, err
+	}
+	if b, err := data.Marshal(); err == nil {
+		_ = h.redisClient.Set(ctx, key, b, HPACacheTime).Err()
+	}
+	return data, nil
+}
+
+func (h *k8sHPA) UpdateV2beta2HPA(
+	ctx context.Context,
+	client kubernetes.Interface,
+	namespace v1Core.Namespace,
+	clusterID uuid.UUID,
+	hpa *v2beta2.HorizontalPodAutoscaler,
+) (*v2beta2.HorizontalPodAutoscaler, error) {
+	key := fmt.Sprintf("hpa_v2_beta_2_list_cluster_%s_ns_%s", clusterID, namespace.Name)
+	data, err := client.
+		AutoscalingV2beta2().
+		HorizontalPodAutoscalers(namespace.Name).
+		Update(
+			ctx,
+			hpa,
+			v1Option.UpdateOptions{
+				FieldManager: constant.K8sHPAUpdateFieldManager,
+			},
+		)
+	if err != nil {
+		return nil, err
+	}
+	if b, err := data.Marshal(); err == nil {
+		_ = h.redisClient.Set(ctx, key, b, HPACacheTime).Err()
+	}
+	return data, nil
+}
+
+func (h *k8sHPA) UpdateV2beta1HPA(
+	ctx context.Context,
+	client kubernetes.Interface,
+	namespace v1Core.Namespace,
+	clusterID uuid.UUID,
+	hpa *v2beta1.HorizontalPodAutoscaler,
+) (*v2beta1.HorizontalPodAutoscaler, error) {
+	key := fmt.Sprintf("hpa_v2_beta_1_list_cluster_%s_ns_%s", clusterID, namespace.Name)
+	data, err := client.
+		AutoscalingV2beta1().
+		HorizontalPodAutoscalers(namespace.Name).
+		Update(
+			ctx,
+			hpa,
+			v1Option.UpdateOptions{
+				FieldManager: constant.K8sHPAUpdateFieldManager,
+			},
+		)
+	if err != nil {
+		return nil, err
+	}
+	if b, err := data.Marshal(); err == nil {
+		_ = h.redisClient.Set(ctx, key, b, HPACacheTime).Err()
+	}
+	return data, nil
 }

c09c6b4d4ecb4eb9653cb3d742c18a86d70a2b4b
Dipta Dwiyantoro <dipta.dwiyantoro@tokopedia.com>
2022-04-08 12:56:18 +0700

    Update response

--- kubeEP-BE/internal/entity/response/statistic.go
+++ kubeEP-BE/internal/entity/response/statistic.go
@@ -4,10 +4,10 @@ import "time"
 
 type NodePoolStatus struct {
 	CreatedAt time.Time `json:"created_at"`
-	Count     int32
+	Count     int32     `json:"count"`
 }
 
 type HPAStatus struct {
 	CreatedAt time.Time `json:"created_at"`
-	Count     int32
+	Count     int32     `json:"count"`
 }
